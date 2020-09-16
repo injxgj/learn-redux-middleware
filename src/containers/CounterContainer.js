554f76d13af19988a5957cf5797b0b817cb098f9
이경준 <gyoungjun_lee@tmax.co.kr>
2020-09-16 13:27:40 +0900

    [feat] 간단한 thunk 적용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,14 +1,14 @@
 import React, { useCallback } from 'react';
 import Counter from '../components/Counter';
 import { useSelector, useDispatch } from 'react-redux';
-import { increase, decrease } from '../modules/counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 
 function CounterContainer() {
   const number = useSelector(({ counter }) => counter);
   const dispatch = useDispatch();
 
-  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
-  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  const onIncrease = useCallback(() => dispatch(increaseAsync()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decreaseAsync()), [dispatch]);
 
   return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 }
