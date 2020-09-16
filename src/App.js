4e83a07370f0e62349ec358b7f7c1662eca1dd52
이경준 <gyoungjun_lee@tmax.co.kr>
2020-09-16 14:38:23 +0900

    [feat] post 관련 Container, Presentation 컴포넌트 추가 및 App.js에 반영

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Counter from './components/Counter';
-import CounterContainer from './containers/CounterContainer';
+// import Counter from './components/Counter';
+// import CounterContainer from './containers/CounterContainer';
+import PostListContainer from './containers/PostListContainer';
 
 function App() {
-  return <CounterContainer />;
+  // return <CounterContainer />;
+  return <PostListContainer />;
 }
 
 export default App;
--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -5,40 +5,45 @@ export const createPromiseThunk = (type, promiseCreator) => {
   // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성됨.
   // 만약 여러 종류의 파라미터를 전달해야하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
   // EX: writeComment({ postId: 1, text: '댓글 내용' });
-  return param => dispatch => {
+  return (param) => async (dispatch) => {
     // 요청 시작
     dispatch({ type, param });
     try {
       // 결과물의 이름을 payload라는 이름으로 통일시킵니다.
       const payload = await promiseCreator(param);
-      dispatch({type: SUCCESS, payload}); // 성공
+      dispatch({ type: SUCCESS, payload }); // 성공
     } catch (e) {
-      dispatch({ type: ERROR, patload: e, error: true}) // 실패
+      dispatch({ type: ERROR, patload: e, error: true }); // 실패
     }
   };
 };
-
 
 // 리듀서에서 사용 할 수 있는 여러 유틸 함수들입니다.
 export const reducerUtils = {
   // 초기 상태. 초기 data 값은 기본적으로 null 이지만 바꿀 수도 있음.
   initial: (initialData = null) => ({
-    loading:false,
+    loading: false,
     data: initialData,
-    error: null
+    error: null,
   }),
 
   // 로딩중 상태. prevState의 경우엔 기본값은 null 이지만
-  // 따로 값을 지정하면 null로 바꾸지 않고 다른 값을 유지시킬 수 있음.
+  // 따로 값을 지정하면 null 로 바꾸지 않고 다른 값을 유지시킬 수 있습니다.
   loading: (prevState = null) => ({
+    loading: true,
+    data: prevState,
+    error: null,
+  }),
+  // 성공 상태
+  success: (payload) => ({
     loading: false,
     data: payload,
-    error: null
+    error: null,
   }),
   // 실패 상태
-  error: error => ({
+  error: (error) => ({
     loading: false,
     data: null,
-    error: error
-  })
+    error: error,
+  }),
 };
